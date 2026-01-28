df72b2ac793ce9ef741296556688d5df96d8bad9
Dogan Alici <doganalici2144@gmail.com>
2026-01-28 20:48:30 +0300

    Day 1 - Added ITaskRepository interface

---
+++ TaskFlow.DataAccess/Interfaces/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskFlow.Entities;
+
+namespace TaskFlow.DataAccess.Interfaces
+{
+    public interface ITaskRepository
+    {
+        List<TaskItem> GetAll();
+        void Add(TaskItem task);
+        void Update(TaskItem task);
+        void Delete(int id);
+    }
+}
